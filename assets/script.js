da7154f4963a995f8c430b3197944103f3f1af87
lydiabarham <lcbarham9@gmail.com>
2023-12-08 19:58:30 +0000

    add time blocks to site using javascript rather than hard code and begin to alter css

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,35 @@ const updateDateTime = () => {
 };
 updateDateTime();
 setInterval(updateDateTime, 1000);
+
+const timeBlockArray = [
+    8, 9, 10, 11, 12, 13, 14, 15, 16, 17
+];
+
+for (let i = 0; i< timeBlockArray.length; i++) {
+    let timeBlock = $("<div>");
+    timeBlock.addClass("time-block");
+    timeBlock.attr("index[i]"); 
+    $(".container").append(timeBlock);
+    let saveButton = $("<button>");
+    saveButton.addClass("save-button");
+    timeBlock.append(saveButton);
+}
+
+
+/*const currentHour = dayjs().isSame($("#time"), "hour");
+const displayCurrentHour = () => {
+    currentHour.text("it's now");
+    currentHour.css("color", "blue");
+};
+
+let hour = $("#time")
+let currentHour = dayjs().hour($("#time"))
+if (dayjs(currentHour, "hour").isSame(dayjs(), "hour")){
+hour.text("it's now")
+}
+
+
+let hour = dayjs().set('hour', 8+i);
+    timeBlock.text(hour);
+ */
