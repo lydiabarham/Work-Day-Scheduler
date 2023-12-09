ccc95e90097c017eec3cff9093696a6c9125a679
lydiabarham <lcbarham9@gmail.com>
2023-12-09 08:39:16 +0000

    finish creating schedule time blocks and formatting with dynamic creation in js

--- assets/script.js
+++ assets/script.js
@@ -12,17 +12,36 @@ const timeBlockArray = [
 ];
 
 for (let i = 0; i< timeBlockArray.length; i++) {
+    //create timeblocks
     let timeBlock = $("<div>");
     timeBlock.addClass("time-block");
     timeBlock.attr("index[i]"); 
     $(".container").append(timeBlock);
+    //create save button
     let saveButton = $("<button>");
     saveButton.addClass("save-button");
     timeBlock.append(saveButton);
+    const buttonText = $("<span>");
+    buttonText.addClass("button-text");
+    buttonText.text("Save");
+    saveButton.append(buttonText);
+    //set hour
+    let hourDisplay = $("<span>");
+    hourDisplay.addClass("hour-display");
+    let hour = dayjs().set('hour', 8+i).format("hh[:00]a");
+    hourDisplay.text(hour);
+    timeBlock.append(hourDisplay);
 }
 
+/*
+for (let i = 0; i< timeBlockArray.length; i++) {
+    let hourDisplay = $("<span>")
+    let hour = dayjs().set('hour', 8+i);
+    hourDisplay.text(hour);
+    timeBlock.append(hourDisplay);
+}
 
-/*const currentHour = dayjs().isSame($("#time"), "hour");
+const currentHour = dayjs().isSame($("#time"), "hour");
 const displayCurrentHour = () => {
     currentHour.text("it's now");
     currentHour.css("color", "blue");
@@ -35,6 +54,5 @@ hour.text("it's now")
 }
 
 
-let hour = dayjs().set('hour', 8+i);
-    timeBlock.text(hour);
+
  */
