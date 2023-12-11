1caecb22cbb075793cb9d2b8da79890ab450f325
lydiabarham <lcbarham9@gmail.com>
2023-12-11 17:07:16 +0000

    complete javascript for schedule's functionality

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// set current day/time 
 const currentDay = $('#currentDay');
 const updateDateTime = () => {
     const currentTime = dayjs().format('MMM, DD YYYY HH:mm:ss');
@@ -7,69 +8,71 @@ const updateDateTime = () => {
 updateDateTime();
 setInterval(updateDateTime, 1000);
 
-const timeBlockArray = [
-    8, 9, 10, 11, 12, 13, 14, 15, 16, 17
-];
+const timeBlockArray = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 for (let i = 0; i < timeBlockArray.length; i++) {
-    //create timeblocks
+    // create time blocks
     let timeBlock = $("<div>");
     timeBlock.addClass("time-block");
-    timeBlock.attr("index[i]");
+    timeBlock.attr("index", i);
     $(".container").append(timeBlock);
-    //create save button
-    let saveButton = $("<button>");
+
+    // set hour display 
+    let hourDisplay = $("<span>");
+    hourDisplay.addClass("hour-display");
+    let hour = dayjs().set('hour', 8 + i).format("HH[:00]");
+    hourDisplay.text(hour);
+    timeBlock.append(hourDisplay);
+    
+    // use if statement to show past, present and future
+    const currentHour = dayjs().format("HH[:00]");
+    
+    if (hour < currentHour) {
+        timeBlock.addClass("past");
+    } else if (hour === currentHour) {
+        timeBlock.addClass("present");
+    } else {
+        timeBlock.addClass("future");
+    }
+
+    // create save button 
+    const saveButton = $("<button>");
     saveButton.addClass("save-button");
     timeBlock.append(saveButton);
     const buttonText = $("<span>");
     buttonText.addClass("button-text");
     buttonText.text("Save");
     saveButton.append(buttonText);
-    //set hour
-    let hourDisplay = $("<span>");
-    hourDisplay.addClass("hour-display");
-    let hour = dayjs().set('hour', 8 + i).format("HH[:00]");
-    hourDisplay.text(hour);
-    timeBlock.append(hourDisplay);
 
-    const currentHour = dayjs().format("HH[:00]");
+    // create input element 
+    const userInputEl = $("<input>");
+    userInputEl.addClass("input-box");
+    userInputEl.attr("name", "user-input");
+    userInputEl.attr("type", "text");
+    userInputEl.attr("placeholder", "Create event:")
+    timeBlock.append(userInputEl);
 
-    console.log(hour);
-    console.log(currentHour);
-
-    if (hour < currentHour) {
-        timeBlock.addClass("past")
-    } else if (hour === currentHour) {
-        timeBlock.addClass("present");
-    } else {
-        timeBlock.addClass("future");
-    }
+    // add event listener 
+    saveButton.on("click", function () {
+        saveInput(userInputEl, i);
+        printInput(i);
+    });
 }
 
-// Get the current time block
+// save input to local storage 
+const saveInput = function (inputEl, index) {
+    let savedInput = inputEl.val();
+    localStorage.setItem(`input-${index}`, JSON.stringify(savedInput));
+}
 
-// Compare the time of the current block with the current time
-
-
-
-/*
-if (dayjs(currentHour).isAfter(dayjs(), "hour")) {
-     currentBlock.css("background-color", "blue");
- } else {
-
-
-const currentHour = dayjs().isSame($("#time"), "hour");
-const displayCurrentHour = () => {
-    currentHour.text("it's now");
-    currentHour.css("color", "blue");
+// retrieve input from local storage 
+const printInput = function (index) {
+    const inputList = $("<ol>");
+    inputList.addClass("input-list");
+    const inputListItem = $('<li>');
+    inputListItem.addClass("input-list-item");
+    const inputText = localStorage.getItem(`input-${index}`);
+    inputListItem.text(inputText);
+    $(`.time-block[index=${index}]`).append(inputList); 
+    inputList.append(inputListItem);
 };
-
-let hour = $("#time")
-let currentHour = dayjs().hour($("#time"))
-if (dayjs(currentHour, "hour").isSame(dayjs(), "hour")){
-hour.text("it's now")
-}
-
-
-
- */
