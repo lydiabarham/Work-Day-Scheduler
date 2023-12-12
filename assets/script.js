f72d1c5a09edd221c147ea6a3adf8ddf62f98a60
lydiabarham <lcbarham9@gmail.com>
2023-12-12 16:32:07 +0000

    upload to test if local storage persist functions on deployed page

--- assets/script.js
+++ assets/script.js
@@ -1,96 +1,104 @@
-// set current day/time 
-const currentDay = $('#currentDay');
-const updateDateTime = () => {
-    const currentTime = dayjs().format('MMM, DD YYYY HH:mm:ss');
-    currentDay.text(currentTime);
-    currentDay.css("color", "#CC5500");
-};
-updateDateTime();
-setInterval(updateDateTime, 1000);
+$(document).ready(function () {
+    // set current day/time 
+    const currentDay = $('#currentDay');
+    const updateDateTime = () => {
+        const currentTime = dayjs().format('MMM, DD YYYY HH:mm:ss');
+        currentDay.text(currentTime);
+        currentDay.css("color", "#CC5500");
+    };
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
 
-const timeBlockArray = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+    const timeBlockArray = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-for (let i = 0; i < timeBlockArray.length; i++) {
-    // create time blocks
-    let timeBlock = $("<div>");
-    timeBlock.addClass("time-block");
-    timeBlock.attr("index", i);
-    $(".container").append(timeBlock);
+    // save input to local storage 
+    const saveInput = function (inputEl, index) {
+        let savedInput = inputEl.val();
+        localStorage.setItem(`input-${index}`, savedInput);
+    };
 
-    // set hour display 
-    let hourDisplay = $("<span>");
-    hourDisplay.addClass("hour-display");
-    let hour = dayjs().set('hour', 8 + i).format("HH[:00]");
-    hourDisplay.text(hour);
-    timeBlock.append(hourDisplay);
+    // retrieve input from local storage 
+    const printInput = function (index) {
+        const inputList = $("<ul>");
+        inputList.addClass("input-list");
+        const inputListItem = $('<li>');
+        inputListItem.addClass("input-list-item");
+        const inputText = localStorage.getItem(`input-${index}`);
+        inputListItem.text(inputText);
+        listCheckbox = $("<input>");
+        listCheckbox.attr("type", "checkbox");
+        listCheckbox.addClass("list-checkbox");
+        inputListItem.append(listCheckbox);
+        $(`.time-block[index=${index}]`).append(inputList);
+        inputList.append(inputListItem);
+    };
 
-    // use if statement to show past, present and future
-    const currentHour = dayjs().format("HH[:00]");
-
-    if (hour < currentHour) {
-        timeBlock.addClass("past");
-    } else if (hour === currentHour) {
-        timeBlock.addClass("present");
-    } else {
-        timeBlock.addClass("future");
+    // read local storage
+    for (let i = 0; i < timeBlockArray.length; i++) {
+        printInput(i);
     }
 
-    // create save button 
-    const saveButton = $("<button>");
-    saveButton.addClass("save-button");
-    timeBlock.append(saveButton);
-    const buttonText = $("<span>");
-    buttonText.addClass("button-text");
-    buttonText.text("Save");
-    saveButton.append(buttonText);
+    for (let i = 0; i < timeBlockArray.length; i++) {
+        // create time blocks
+        let timeBlock = $("<div>");
+        timeBlock.addClass("time-block");
+        timeBlock.attr("index", i);
+        $(".container").append(timeBlock);
 
-    // create input element 
-    const userInputEl = $("<input>");
-    userInputEl.addClass("input-box");
-    userInputEl.attr("name", "user-input");
-    userInputEl.attr("type", "text");
-    userInputEl.attr("placeholder", "Create event:")
-    timeBlock.append(userInputEl);
+        // set hour display 
+        let hourDisplay = $("<span>");
+        hourDisplay.addClass("hour-display");
+        let hour = dayjs().set('hour', 8 + i).format("HH[:00]");
+        hourDisplay.text(hour);
+        timeBlock.append(hourDisplay);
 
-    // add event listener 
-    saveButton.on("click", function () {
-        const userInputValue = userInputEl.val().trim();
+        // use if statement to show past, present and future
+        const currentHour = dayjs().format("HH[:00]");
 
-        if (userInputValue !== "") {
-            saveInput(userInputEl, i);
-            printInput(i);
-            userInputEl.val("");
-            userInputEl.attr("placeholder", "Create event:");
+        if (hour < currentHour) {
+            timeBlock.addClass("past");
+        } else if (hour === currentHour) {
+            timeBlock.addClass("present");
         } else {
-
+            timeBlock.addClass("future");
         }
+
+        // create save button 
+        const saveButton = $("<button>");
+        saveButton.addClass("save-button");
+        timeBlock.append(saveButton);
+        const buttonText = $("<span>");
+        buttonText.addClass("button-text");
+        buttonText.text("Save");
+        saveButton.append(buttonText);
+
+        // create input element 
+        const userInputEl = $("<input>");
+        userInputEl.addClass("input-box");
+        userInputEl.attr("name", "user-input");
+        userInputEl.attr("type", "text");
+        userInputEl.attr("placeholder", "Create event:")
+        timeBlock.append(userInputEl);
+
+        // add event listener 
+        saveButton.on("click", function () {
+            const userInputValue = userInputEl.val().trim();
+
+            if (userInputValue !== "") {
+                saveInput(userInputEl, i);
+                printInput(i);
+                userInputEl.val("");
+                userInputEl.attr("placeholder", "Create event:");
+            } else {
+
+            }
+        });
+    }
+
+    // add event listener on checkbox
+    $(".time-block").on("click", ".list-checkbox", function () {
+        let thisListItem = $(this).closest(".input-list-item");
+        thisListItem.remove();
     });
-}
-
-// save input to local storage 
-const saveInput = function (inputEl, index) {
-    let savedInput = inputEl.val();
-    localStorage.setItem(`input-${index}`, savedInput);
-};
-
-// retrieve input from local storage 
-const printInput = function (index) {
-    const inputList = $("<ul>");
-    inputList.addClass("input-list");
-    const inputListItem = $('<li>');
-    inputListItem.addClass("input-list-item");
-    const inputText = localStorage.getItem(`input-${index}`);
-    inputListItem.text(inputText);
-    listCheckbox = $("<input>");
-    listCheckbox.attr("type", "checkbox");
-    listCheckbox.addClass("list-checkbox");
-    inputListItem.append(listCheckbox);
-    $(`.time-block[index=${index}]`).append(inputList);
-    inputList.append(inputListItem);
-};
-
-// add event listener on checkbox
-$(".time-block").on("click", ".list-checkbox", function () {
-    let thisListItem = $(this).closest(".input-list-item");
-    thisListItem.remove();
 });
+
